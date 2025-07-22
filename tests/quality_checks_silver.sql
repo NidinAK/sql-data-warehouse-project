ee74155c5cb2129657afe046413ef34c4b26560a
NidinAK <118205458+NidinAK@users.noreply.github.com>
2025-07-22 21:36:59 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -44,3 +44,41 @@ SELECT
 	DISTINCT cst_marital_status
 FROM	
 	silver.crm_cust_info;
+
+-- ============================================================
+-- Checking silver.crm_prd_info
+
+### Note: Yet to complete the query 
+-- ============================================================
+
+-- Check for unwanted spaces 
+-- Expectation: No result
+
+SELECT 
+	prd_nm
+FROM
+	bronze.crm_prd_info
+WHERE 
+	prd_nm != TRIM(prd_nm);
+
+-- Check for nulls or negative numbers
+-- Expectation: No result
+SELECT
+	prd_cost
+FROM
+	bronze.crm_prd_info
+WHERE
+	prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT	
+	DISTINCT prd_line
+FROM
+	bronze.crm_prd_info
+
+-- Check for invalid date orders
+SELECT *
+FROM 
+	bronze.crm_prd_info
+WHERE
+	prd_end_dt < prd_start_dt;
