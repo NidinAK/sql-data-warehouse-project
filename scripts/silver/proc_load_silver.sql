598f420bfaa9131cde06a235c28023444ba1ac58
Nidin A <118205458+NidinAK@users.noreply.github.com>
2025-07-24 21:19:09 +0530

    uploaded erp_loc_a101

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -150,4 +150,22 @@ FROM
 	bronze.erp_cust_az12
 
 
+-- ======================================================================
+-- silver.erp_loc_a101
+-- ======================================================================
+	
+INSERT INTO silver.erp_loc_a101(
+	cid, cntry)
+
+SELECT 
+	REPLACE(cid, '-','') AS cid,
+	CASE 
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) IN ('USA', 'US') THEN 'United States'
+		WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		ELSE TRIM(cntry)    -- Normalize and handling missing values
+	END AS cntry
+FROM
+	bronze.erp_loc_a101
+
 
