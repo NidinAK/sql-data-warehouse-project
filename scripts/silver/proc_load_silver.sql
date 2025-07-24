000bf185cfbcab11ee31c52a8c1d141473e727ee
Nidin A <118205458+NidinAK@users.noreply.github.com>
2025-07-24 21:03:27 +0530

    proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -43,7 +43,6 @@ FROM (
 -- silver.crm_prd_info
 -- ==================================================================
 
-
 INSERT INTO silver.crm_prd_info (
 	prd_id,		
 	cat_id,
@@ -121,4 +120,34 @@ SELECT
 		ELSE sls_price
 	END AS sls_price
 FROM bronze.crm_sales_details
+
+
+-- ======================================================================
+-- silver.erp_cust_az12
+-- ==================================================================
+
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)	
+
+SELECT
+	CASE 
+		WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+		ELSE cid
+	END AS cid,
+	CASE 
+		WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate
+	END AS bdate,
+	CASE 
+		WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
+		WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+		ELSE 'n/a'
+	END AS gen
+FROM
+	bronze.erp_cust_az12
+
+
 
